dedca6858fbcf260f32916ededf96f301c6795f6
Phelipe-Souza <phelipe691@gmail.com>
2021-04-25 10:45:11 -0300

    Teste FrontEnd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroProdutosComponent } from './cadastro-produtos/cadastro-produtos.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:MenuComponent},
+  {path: 'menu', component:MenuComponent},
+  {path: 'produtos', component:ProdutosComponent},
+  {path: 'cadastrar', component: CadastroProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
